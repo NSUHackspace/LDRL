d5cf545ae423cc72b344e621260a3aa1f8eabbee
Alex1um <nothingorno@gmail.com>
2022-07-28 17:15:55 +0700

    added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Kicker',
+    version='1.0',
+    packages=['gym', 'numpy', 'pybullet'],
+    url='',
+    license='',
+    author='LDRL',
+    author_email='',
+    description=''
+)
