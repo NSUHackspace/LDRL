43135623b6a72beb8f4ee7306abb51ac88cd40d0
Vsevolod Prokopenko <PPVV0033@gmail.com>
2022-08-04 14:37:41 +0000

    added inline comments for gym_env_test.py example

--- examples/gym_env_test.py
+++ examples/gym_env_test.py
@@ -3,19 +3,29 @@ from pybullet import getKeyboardEvents
 from kicker.reset_functions import camera_reset
 
 
+# creating Gym environment
 env = KickerEnv()
 
+# hotkeys to reset environment and camera
 KEY_R = ord("r")
 KEY_C = ord("c")
 
 while True:
+    # reseting keys
     getKeyboardEvents(env.pb_connection)
     keys = None
+    
+    # reseting environment
     env.reset()
     done = False
     for _ in " " * 10000:
+        # getting pressed keys
         keys = getKeyboardEvents(env.pb_connection)
+        
+        # environment step with random actions
         action_space, rw, done, _ = env.step(env.action_space.sample())
+        
+        # checking pressed keys
         if done or KEY_R in keys:
             break
         if KEY_C in keys:
