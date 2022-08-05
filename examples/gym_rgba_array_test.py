68a6b06de4c44ad663ee439aa7b590bbcdde1e72
Alex1um <nothingorno@gmail.com>
2022-08-05 16:09:25 +0700

    added resolution parameter in gym_rgba_array_test.py example. Testing pip

--- examples/gym_rgba_array_test.py
+++ examples/gym_rgba_array_test.py
@@ -1,15 +1,17 @@
-from kicker.gym_env import KickerEnv
+from src.kicker.gym_env import KickerEnv
 import pybullet as pb
 from numpngw import write_apng
 import numpy as np
 
 
+resolution = (600, 400)
+
 # creating Gym environment
-env = KickerEnv(bullet_connection_type=pb.DIRECT, render_mode="rgba_array")
+env = KickerEnv(bullet_connection_type=pb.DIRECT, render_mode="rgba_array", render_resolution=resolution)
 
 env.reset()
 start_img = env.render()
-start_img = [np.array(start_img[0], dtype="uint8").reshape((800, 1024, 4))]
+start_img = [np.array(start_img[0], dtype="uint8").reshape((*resolution, 4))]
 
 # rgba = np
 write_apng("start.png", start_img, delay=20)
