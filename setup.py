b07c8ac26b906a1ad94116be523bd1acaa022da1
Alex1um <nothingorno@gmail.com>
2022-08-05 16:17:12 +0700

    testing src edit

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='Kicker-in-the-Gym',
-    version='0.0.2',
+    version='0.0.3',
     package_dir={'kicker': 'src/kicker'},
     packages=find_packages("./src", include=['kicker', 'kicker.*']),
     package_data={'kicker': ['assets/*.urdf']},
