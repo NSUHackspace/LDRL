58f52ccfddb7b0f5150bbfe72471ddc4e512c1c9
Eugene Mikhantiev <eugene.mikhantiev@turation.io>
2022-08-12 00:54:39 +0700

    Vector enviroment

--- examples/ppo_train.py
+++ examples/ppo_train.py
@@ -15,21 +15,45 @@ from kicker.callbacks import GoalCallback
 from stable_baselines3.common.logger import configure
 from kicker.wrappers import FlattenAction
 from kicker.callbacks import GoalCallback
+from stable_baselines3.common.vec_env import DummyVecEnv, SubprocVecEnv
 import os
+from typing import Callable
+from stable_baselines3.common.utils import set_random_seed
+
+
+def make_env(rank: int, seed: int = 0) -> Callable:
+    """
+    Utility function for multiprocessed env.
+
+    :param env_id: (str) the environment ID
+    :param num_env: (int) the number of environment you wish to have in subprocesses
+    :param seed: (int) the inital seed for RNG
+    :param rank: (int) index of the subprocess
+    :return: (Callable)
+    """
+
+    def _init() -> gym.Env:
+        env = FlattenAction(
+            FlattenObservation(
+                KickerEnv(
+                    bullet_connection_type=pb.DIRECT,
+                    max_steps=10000,
+                    ai_function=create_rotate_to_target_bot,
+                )
+            )
+        )
+        env = FrameStack(env, 2)
+        env.seed(seed + rank)
+        return env
+
+    set_random_seed(seed)
+    return _init
 
 
 def main():
-    # creating Gym environment
-    env = FlattenAction(
-        FlattenObservation(
-            KickerEnv(
-                bullet_connection_type=pb.DIRECT,
-                max_steps=10000,
-                ai_function=create_rotate_to_target_bot,
-            )
-        )
-    )
-    env = FrameStack(env, 2)
+    num_cpu = 10  # Number of processes to use
+    # Create the vectorized environment
+    env = SubprocVecEnv([make_env(i) for i in range(num_cpu)])
 
     model_dir = "model/ppo"
     if not os.path.exists(model_dir):
