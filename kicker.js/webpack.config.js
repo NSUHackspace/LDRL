4207d898f193644e0085f6c76e14cb5402dbe195
Alex1um <nothingorno@gmail.com>
2022-08-11 13:05:07 +0700

    js scene start

---
+++ kicker.js/webpack.config.js
@@ -0,0 +1,20 @@
+
+module.exports = {
+    mode: "development", // "production" | "development" | "none"
+    // Chosen mode tells webpack to use its built-in optimizations accordingly.
+    // defaults to ./src
+    optimization: {
+        // chunkIds: "size",
+        // method of generating ids for chunks
+        // moduleIds: "size",
+        // method of generating ids for modules
+        // mangleExports: "size",
+        // rename export names to shorter names
+        minimize: false,
+        // minimize the output files
+        /* Advanced optimizations (click to show) */
+    },
+    /* Advanced configuration (click to show) */
+    /* Advanced caching configuration (click to show) */
+    /* Advanced build configuration (click to show) */
+}
