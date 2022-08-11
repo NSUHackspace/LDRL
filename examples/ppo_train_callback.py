e0c27246e8f7d6ac75a1d77bcc394e6014d9d364
Lazymanul <noname@from.nowhere>
2022-08-11 15:42:05 +0700

    test callback

---
+++ examples/ppo_train_callback.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+
+from kicker.gym_env import KickerEnv
+import pybullet as pb
+from kicker.reset_functions import camera_reset
+from stable_baselines3 import PPO
+from gym.wrappers import FlattenObservation
+from gym import ActionWrapper
+import gym
+from gym import spaces
+from kicker.reward_functions import simple_reward
+import os
+from stable_baselines3.common.callbacks import CheckpointCallback
+
+class FlattenAction(ActionWrapper):
+    """Action space wrapper that flattens the actions."""
+
+    def __init__(self, env: gym.Env):
+        """Flattens the observations of an environment.
+
+        Args:
+            env: The environment to apply the wrapper
+        """
+        super().__init__(env)
+        self.action_space = spaces.flatten_space(env.action_space)
+
+    def action(self, action):
+        """Flattens an action.
+
+        Args:
+            action: The action to flatten
+
+        Returns:
+            The flattened action
+        """
+        return spaces.unflatten(self.env.action_space, action)
+
+
+def main():
+
+    # creating Gym environment
+    env = FlattenAction(
+        FlattenObservation(
+            KickerEnv(bullet_connection_type=pb.GUI, ai_function=None, max_steps=10000, reward_function=simple_reward)
+        )
+    )
+
+    checkpoint_callback = CheckpointCallback(save_freq=1000, save_path='./logs/',
+                                         name_prefix='rl_model')    
+
+    model = PPO("MlpPolicy", env, verbose=1)
+    model.learn(total_timesteps=1E+6, callback=checkpoint_callback)
+
+    # obs = env.reset()
+    # for i in range(1000):
+    #     action, _states = model.predict(obs, deterministic=True)
+    #     obs, reward, done, info = env.step(action)
+    #     env.render()
+    #     if done:
+    #         obs = env.reset()
+
+    env.close()
+
+
+if __name__ == "__main__":
+    main()
